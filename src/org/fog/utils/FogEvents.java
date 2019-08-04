5099b726557072f3d0aac6bbbaaddcedf3bbb95a
JoseCVieira <josecarlosvieira@tecnico.ulisboa.pt>
2019-08-05 00:21:41 +0100

    Removing some unecessary events

--- src/org/fog/utils/FogEvents.java
+++ src/org/fog/utils/FogEvents.java
@@ -10,32 +10,24 @@ package org.fog.utils;
 public class FogEvents {
 	private static final int BASE = 50;
 	
+	// Basic events
 	public static final int TUPLE_ARRIVAL = BASE + 1;
 	public static final int LAUNCH_MODULE = BASE + 2;
-	public static final int RELEASE_OPERATOR = BASE + 3;
-	public static final int APP_SUBMIT = BASE + 4;
-	public static final int CALCULATE_INPUT_RATE = BASE + 5;
-	public static final int CALCULATE_UTIL = BASE + 6;
-	public static final int UPDATE_RESOURCE_USAGE = BASE + 7;
-	public static final int ADAPTIVE_OPERATOR_REPLACEMENT = BASE + 8;
-	public static final int GET_RESOURCE_USAGE = BASE + 9;
-	public static final int RESOURCE_USAGE = BASE + 10;
-	public static final int CONTROL_MSG_ARRIVAL = BASE + 11;
-	public static final int UPDATE_TUPLE_QUEUE = BASE + 12;
-	public static final int ACTUATOR_JOINED = BASE + 13;
-	public static final int STOP_SIMULATION = BASE + 14;
-	public static final int SEND_PERIODIC_TUPLE = BASE + 15;
-	public static final int RESOURCE_MGMT = BASE + 16;
-	public static final int INITIALIZE_SENSOR = BASE + 17;
-	public static final int EMIT_TUPLE = BASE + 18;
+	public static final int APP_SUBMIT = BASE + 3;
+	public static final int UPDATE_TUPLE_QUEUE = BASE + 4;
+	public static final int ACTUATOR_JOINED = BASE + 5;
+	public static final int STOP_SIMULATION = BASE + 6;
+	public static final int SEND_PERIODIC_TUPLE = BASE + 7;
+	public static final int RESOURCE_MGMT = BASE + 8;
+	public static final int EMIT_TUPLE = BASE + 9;
 	
 	// Mobile events
-	public static final int UPDATE_PERIODIC_MOVEMENT = BASE + 19;
-	public static final int CONNECTION_LOST = BASE + 20;
-	public static final int UPDATE_TOPOLOGY = BASE + 21;
-	public static final int MIGRATION = BASE + 22;
-	public static final int FINISH_MIGRATION = BASE + 23;
-	public static final int FINISH_SETUP_MIGRATION = BASE + 24;
-	public static final int UPDATE_VM_POSITION = BASE + 25;
+	public static final int UPDATE_PERIODIC_MOVEMENT = BASE + 10;
+	public static final int CONNECTION_LOST = BASE + 11;
+	public static final int UPDATE_TOPOLOGY = BASE + 12;
+	public static final int MIGRATION = BASE + 13;
+	public static final int FINISH_MIGRATION = BASE + 14;
+	public static final int FINISH_SETUP_MIGRATION = BASE + 15;
+	public static final int UPDATE_VM_POSITION = BASE + 16;
 	
 }
