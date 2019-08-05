a4d15d82fd46b2df5ecf6d333cb427836055fdcd
JoseCVieira <josecarlosvieira@tecnico.ulisboa.pt>
2019-08-05 15:45:14 +0100

    Organizing and commenting code

--- src/org/fog/placement/algorithm/bf/BruteForce.java
+++ src/org/fog/placement/algorithm/bf/BruteForce.java
@@ -1,6 +1,7 @@
-package org.fog.placement.algorithm.overall.bf;
+package org.fog.placement.algorithm.bf;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.fog.application.Application;
@@ -11,197 +12,266 @@ import org.fog.entities.FogDevice;
 import org.fog.entities.Sensor;
 import org.fog.placement.algorithm.Algorithm;
 import org.fog.placement.algorithm.Job;
-import org.fog.placement.algorithm.overall.util.AlgorithmUtils;
+import org.fog.placement.algorithm.util.AlgorithmUtils;
+import org.fog.placement.algorithm.util.routing.DijkstraAlgorithm;
+import org.fog.placement.algorithm.util.routing.Edge;
+import org.fog.placement.algorithm.util.routing.Graph;
+import org.fog.placement.algorithm.util.routing.Vertex;
 
+/**
+ * Class in which defines and executes the brute force algorithm.
+ * 
+ * @author  José Carlos Ribeiro Vieira @ Instituto Superior Técnico (IST)
+ * @since   July, 2019
+ */
 public class BruteForce extends Algorithm {
 	private Job bestSolution;
 	private double bestCost;
 	private int iteration;
+	private long start, finish;
+	
+	private List<Vertex> nodes;
+	private DijkstraAlgorithm dijkstra;
 	
 	public BruteForce(final List<FogDevice> fogDevices, final List<Application> applications,
 			final List<Sensor> sensors, final List<Actuator> actuators) {
 		super(fogDevices, applications, sensors, actuators);
 	}
 	
+	/**
+	 * Executes the brute force algorithm in order to find the best solution (the solution with the lower cost which respects all constraints).
+	 * 
+	 * @return the best solution; can be null
+	 */
 	@Override
 	public Job execute() {
-		bestSolution = null;
-		bestCost = Constants.REFERENCE_COST;
 		iteration = 0;
+		bestCost = Constants.REFERENCE_COST;
+		bestSolution = null;
 		
-		long start = System.currentTimeMillis();
-		solveDeployment(new int[NR_NODES][NR_MODULES], 0);
-		long finish = System.currentTimeMillis();
+		// Time at the beginning of the execution of the algorithm
+		start = System.currentTimeMillis();
+		
+		// Generate a new Dijkstra graph
+		nodes = new ArrayList<Vertex>();
+		List<Edge> edges = new ArrayList<Edge>();
+		
+		for(int i  = 0; i < getNumberOfNodes(); i++) {
+			nodes.add(new Vertex("Node=" + i));
+		}
+		
+		for(int i  = 0; i < getNumberOfNodes(); i++) {
+			for(int j  = 0; j < getNumberOfNodes(); j++) {
+				if(getfLatencyMap()[i][j] < Constants.INF) {
+					 edges.add(new Edge(nodes.get(i), nodes.get(j), 1.0));
+				}
+			}
+        }
+		
+		Graph graph = new Graph(nodes, edges);
+		dijkstra = new DijkstraAlgorithm(graph);
+		
+		// Solve the problem
+		solveModulePlacement(new int[NR_NODES][NR_MODULES], 0);
+		
+		// Time at the end of the execution of the algorithm
+		finish = System.currentTimeMillis();
+		
 		elapsedTime = finish - start;
 		
-		if(Config.PRINT_DETAILS)
-			AlgorithmUtils.printResults(this, bestSolution);
+		if(bestSolution != null && Config.PRINT_DETAILS) {
+	    	AlgorithmUtils.printAlgorithmResults(this, bestSolution);
+		}
 		
 		return bestSolution;
 	}
 	
-	private void solveDeployment(int[][] modulePlacementMap, int index) {
+	/**
+	 * Solves the module placement map.
+	 * 
+	 * @param modulePlacementMap the current binary module placement map
+	 * @param index the index of the module to be filled up
+	 */
+	private void solveModulePlacement(int[][] modulePlacementMap, final int index) {
 		int[][] currentPositionInt = getCurrentPositionInt();
 		
 		for(int i = 0; i < NR_NODES; i++) {
-			if(possibleDeployment[i][index] == 1) {
-				for(int j = 0; j < NR_NODES; j++) {
-					if(j == i)
-						modulePlacementMap[j][index] = 1;
-					else
-						modulePlacementMap[j][index] = 0;
-				}
+			// If its not a valid placement continue
+			if(possibleDeployment[i][index] == 0) continue;
+			
+			// Clear other placements of the module and place it in the correct node index
+			for(int j = 0; j < NR_NODES; j++) {
+				modulePlacementMap[j][index] = j == i ? 1 : 0;
+			}
+			
+			// If its not the final module, solve the next one
+			if(index != NR_MODULES - 1) {
+				solveModulePlacement(modulePlacementMap, index + 1);
+			}
+			
+			// Tries the current module placement map
+			else {
+				int[][] tupleRoutingMap = new int[getNumberOfDependencies()][getNumberOfNodes()];
 				
-				if(index != NR_MODULES - 1)
-					solveDeployment(modulePlacementMap, index + 1);
-				else {
-					//Trying the following placement map
-			        List<Integer> initialNodes = new ArrayList<Integer>();
-			        List<Integer> finalNodes = new ArrayList<Integer>();
-					
-					for(int j = 0; j < NR_MODULES; j++) {
-						for(int z = 0; z < NR_MODULES; z++) {
-							if(getmDependencyMap()[j][z] != 0) {
-								initialNodes.add(Job.findModulePlacement(modulePlacementMap, j));
-								finalNodes.add(Job.findModulePlacement(modulePlacementMap, z));
-							}
+				int tmp = 0;
+				for(int j = 0; j < NR_MODULES; j++) {
+					for(int z = 0; z < NR_MODULES; z++) {
+						if(getmDependencyMap()[j][z] != 0) {
+							tupleRoutingMap[tmp][0] = Job.findModulePlacement(modulePlacementMap, j);
+							tupleRoutingMap[tmp++][NR_NODES-1] = Job.findModulePlacement(modulePlacementMap, z);
 						}
 					}
-					
-					int[][] tupleRoutingMap = convertListToMatrix(initialNodes, finalNodes);
-					
-					initialNodes = new ArrayList<Integer>();
-			        finalNodes = new ArrayList<Integer>();
-			        
-					for(int j = 0; j < NR_MODULES; j++) {
-						if(isFirstOptimization())
-							initialNodes.add(Job.findModulePlacement(modulePlacementMap, j));
-						else
-							initialNodes.add(Job.findModulePlacement(currentPositionInt, j));
-						finalNodes.add(Job.findModulePlacement(modulePlacementMap, j));
-					}
-					
-					int[][] migrationRoutingMap = convertListToMatrix(initialNodes, finalNodes);
-					solveVmRouting(new Job(this, modulePlacementMap, tupleRoutingMap, migrationRoutingMap), 0, 1);
 				}
+		        
+		        int[][] migrationRoutingMap = new int[getNumberOfModules()][getNumberOfNodes()];
+		        
+				for(int j = 0; j < NR_MODULES; j++) {
+					migrationRoutingMap[j][0] = isFirstOptimization() ? Job.findModulePlacement(modulePlacementMap, j) : Job.findModulePlacement(currentPositionInt, j);
+					migrationRoutingMap[j][NR_NODES-1] = Job.findModulePlacement(modulePlacementMap, j);
+				}
+				
+				solveVmRouting(modulePlacementMap, tupleRoutingMap, migrationRoutingMap, 0, 1);
 			}
 		}
 	}
 	
-	private void solveVmRouting(Job job, int row, int col) {
-		int[][] modulePlacementMap = job.getModulePlacementMap();
-		int[][] tupleRoutingMap = job.getTupleRoutingMap();
-		int[][] migrationRoutingMap = job.getMigrationRoutingMap();
-		
+	/**
+	 * Solves the virtual machine routing map.
+	 * 
+	 * @param modulePlacementMap the current binary module placement map
+	 * @param tupleRoutingMap the current tuple routing map
+	 * @param migrationRoutingMap the current virtual machine migration map
+	 * @param row the index of the module to be filled up
+	 * @param col the index of the node to be filled up
+	 */
+	private void solveVmRouting(final int[][] modulePlacementMap, final int[][] tupleRoutingMap,
+			int[][] migrationRoutingMap, final int row, final int col) {
 		int max_r = migrationRoutingMap.length - 1;
 		int max_c = migrationRoutingMap[0].length - 1;
 		
+		// If VM routing matrix is already filled solve the tuple routing matrix
 		if(row == max_r + 1 && col == 1) {
-			solveTupleRouting(new Job(this, modulePlacementMap, tupleRoutingMap, migrationRoutingMap), 0, 1);
+			solveTupleRouting(modulePlacementMap, tupleRoutingMap, migrationRoutingMap, 0, 1);
+			
+		// Otherwise, keep filling the VM routing matrix
 		}else {
 			int previousNode = migrationRoutingMap[row][col-1];
 			
+			// If it already find out the destination just fill the next hops with the destination index
 			if(previousNode == migrationRoutingMap[row][NR_NODES-1]) {
 				migrationRoutingMap[row][col] = previousNode;
 				
+				// If its not the final hop, fill the next hop for the current module/VM
 				if(col < max_c-1)
-					solveVmRouting(new Job(this, modulePlacementMap, tupleRoutingMap, migrationRoutingMap), row, col + 1);
+					solveVmRouting(modulePlacementMap, tupleRoutingMap, migrationRoutingMap, row, col + 1);
+				
+				// Otherwise, fill the next module/VM path
 				else
-					solveVmRouting(new Job(this, modulePlacementMap, tupleRoutingMap, migrationRoutingMap), row + 1, 1);
+					solveVmRouting(modulePlacementMap, tupleRoutingMap, migrationRoutingMap, row + 1, 1);
+				
+			// Otherwise, keep filling the VM routing matrix
 			}else {
 				for(int i = 0; i < NR_NODES; i++) {
-					if(getfLatencyMap()[previousNode][i] < Constants.INF) {
-						
-						boolean valid = true;
-						for(int j = 0; j < col - 1; j++) {
-							if(migrationRoutingMap[row][j] == i && i != previousNode) {
-								valid = false;
-								break;
-							}
-						}
-							
-						if(!valid)
-							continue;
-						
-						migrationRoutingMap[row][col] = i;
+					if(getfLatencyMap()[previousNode][i] == Constants.INF) continue;
+					if(!isValidHop(i, migrationRoutingMap[row][NR_NODES-1], NR_NODES - col)) continue;
 					
-						if(col < max_c-1)
-							solveVmRouting(new Job(this, modulePlacementMap, tupleRoutingMap, migrationRoutingMap), row, col + 1);
-						else
-							solveVmRouting(new Job(this, modulePlacementMap, tupleRoutingMap, migrationRoutingMap), row + 1, 1);
-					}
+					migrationRoutingMap[row][col] = i;
+				
+					// If its not the final hop, fill the next hop for the current module/VM
+					if(col < max_c-1)
+						solveVmRouting(modulePlacementMap, tupleRoutingMap, migrationRoutingMap, row, col + 1);
+					
+					// Otherwise, fill the next module/VM path
+					else
+						solveVmRouting(modulePlacementMap, tupleRoutingMap, migrationRoutingMap, row + 1, 1);
 				}
 			}
 		}
 	}
 	
-	private void solveTupleRouting(Job job, int row, int col) {
-		int[][] modulePlacementMap = job.getModulePlacementMap();
-		int[][] tupleRoutingMap = job.getTupleRoutingMap();
-		int[][] migrationRoutingMap = job.getMigrationRoutingMap();
-		
+	/**
+	 *  Solves the tuple routing map.
+	 * 
+	 * @param modulePlacementMap the current binary module placement map
+	 * @param tupleRoutingMap the current tuple routing map
+	 * @param migrationRoutingMap the current virtual machine migration map
+	 * @param row the index of the dependency to be filled up
+	 * @param col the index of the node to be filled up
+	 */
+	private void solveTupleRouting(final int[][] modulePlacementMap, int[][] tupleRoutingMap,
+			final int[][] migrationRoutingMap, final int row, final int col) {
 		int max_r = tupleRoutingMap.length - 1;
 		int max_c = tupleRoutingMap[0].length - 1;
 		
-		if(row == max_r + 1 && col == 1) {
-			//Trying the following routing map
+		// If tuple routing matrix is already filled analyze the following solution
+		if(row == max_r + 1 && col == 1) {			
+			Job job = new Job(this, modulePlacementMap, tupleRoutingMap, migrationRoutingMap);
 			
 			if(job.getCost() < bestCost) {
 				bestCost = job.getCost();
 				bestSolution = new Job(job);
     			valueIterMap.put(iteration, bestCost);
     			
-    			if(Config.PRINT_BEST_ITER)
+    			if(Config.PRINT_ALGORITHM_BEST_ITER)
     				System.out.println("iteration: " + iteration + " value: " + bestCost);
 			}
+			
 			iteration++;
 			
+		// Otherwise, keep filling the tuple routing matrix
 		}else {
 			int previousNode = tupleRoutingMap[row][col-1];
 			
+			// If it already find out the destination just fill the next hops with the destination index
 			if(previousNode == tupleRoutingMap[row][NR_NODES-1]) {
 				tupleRoutingMap[row][col] = previousNode;
 				
+				// If its not the final hop, fill the next hop for the current tuple
 				if(col < max_c-1)
-					solveTupleRouting(new Job(this, modulePlacementMap, tupleRoutingMap, migrationRoutingMap), row, col + 1);
+					solveTupleRouting(modulePlacementMap, tupleRoutingMap, migrationRoutingMap, row, col + 1);
+				
+				// Otherwise, fill the next tuple path
 				else
-					solveTupleRouting(new Job(this, modulePlacementMap, tupleRoutingMap, migrationRoutingMap), row + 1, 1);
+					solveTupleRouting(modulePlacementMap, tupleRoutingMap, migrationRoutingMap, row + 1, 1);
+				
+				// Otherwise, keep filling the tuple routing matrix
 			}else {
 				for(int i = 0; i < NR_NODES; i++) {
-					if(getfLatencyMap()[previousNode][i] < Constants.INF) {
-						
-						boolean valid = true;
-						for(int j = 0; j < col - 1; j++) {
-							if(tupleRoutingMap[row][j] == i && i != previousNode) {
-								valid = false;
-								break;
-							}
-						}
-							
-						if(!valid)
-							continue;
-						
-						tupleRoutingMap[row][col] = i;
+					if(getfLatencyMap()[previousNode][i] == Constants.INF) continue;
+					if(!isValidHop(i, tupleRoutingMap[row][NR_NODES-1], NR_NODES - col)) continue;
 					
-						if(col < max_c-1)
-							solveTupleRouting(new Job(this, modulePlacementMap, tupleRoutingMap, migrationRoutingMap), row, col + 1);
-						else
-							solveTupleRouting(new Job(this, modulePlacementMap, tupleRoutingMap, migrationRoutingMap), row + 1, 1);
-					}
+					tupleRoutingMap[row][col] = i;
+				
+					// If its not the final hop, fill the next hop for the current tuple
+					if(col < max_c-1)
+						solveTupleRouting(modulePlacementMap, tupleRoutingMap, migrationRoutingMap, row, col + 1);
+					
+					// Otherwise, fill the next tuple path
+					else
+						solveTupleRouting(modulePlacementMap, tupleRoutingMap, migrationRoutingMap, row + 1, 1);
 				}
 			}
 		}
 	}
 	
-	private int[][] convertListToMatrix(List<Integer> initials, List<Integer> finals) {
-	    int[][] ret = new int[initials.size()][NR_NODES];
-	    
-	    for(int i = 0; i < initials.size(); i++) {
-	    	ret[i][0] = initials.get(i);
-	    	ret[i][NR_NODES-1] = finals.get(i);
-	    }
-	    
-	    return ret;
+	/**
+	 * Checks whether the node is valid. It is valid if, and only if, from the current node index towards the final node index
+	 * there is a path which has lower or equal number of hops as the provided maximum distance.
+	 * 
+	 * @param nodeIndex the current node index
+	 * @param finalNodeIndex the final node index
+	 * @param maxDistance the maximum allowed distance
+	 * @return true is it's a valid node. False, otherwise.
+	 */
+	private boolean isValidHop(final int nodeIndex, final int finalNodeIndex, final int maxDistance) {
+			dijkstra.execute(nodes.get(nodeIndex));
+			LinkedList<Vertex> path = dijkstra.getPath(nodes.get(finalNodeIndex));
+			
+			// If path is null, means that both start and finish refer to the same node, thus it can be added
+	        if((path != null && path.size() <= maxDistance) || path == null)
+	        	return true;
+	        
+	        return false;
 	}
 	
 }
