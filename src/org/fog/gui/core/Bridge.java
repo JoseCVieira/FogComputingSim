1180750b595e1a64d95d20652ab57118ee4df7a5
JoseCVieira <josecarlosvieira@tecnico.ulisboa.pt>
2019-08-12 18:00:45 +0100

    Organizing and commenting code

--- src/org/fog/gui/core/Bridge.java
+++ src/org/fog/gui/core/Bridge.java
@@ -14,7 +14,9 @@ import java.util.Map.Entry;
 
 import org.apache.commons.math3.util.Pair;
 import org.fog.application.AppEdge;
+import org.fog.application.AppLoop;
 import org.fog.application.AppModule;
+import org.fog.application.Application;
 import org.fog.application.selectivity.FractionalSelectivity;
 import org.fog.utils.Location;
 import org.fog.utils.Movement;
@@ -26,26 +28,30 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
-public class Bridge {
-	private static Node getNode(Graph graph, String name){
-		for(Node node : graph.getDevicesList().keySet())
-			if(node!=null)
-				if(node.getName().equals(name))
-					return node;
-		return null;
-	}
-
-	// convert from JSON object to Graph object
-	public static Graph jsonToGraph(String fileName){
+/**
+ * Interface used to read/write topologies from/to a JSON file.
+ * 
+ * @author José Carlos Ribeiro Vieira @ Instituto Superior Técnico (IST), Lisbon-Portugal
+ * @since  July, 2019
+ */
+public class Bridge {	
+	/**
+	 * Converts from JSON object to Graph object.
+	 * 
+	 * @param fileName the name of the file to read from
+	 * @return the object which holds the physical topology
+	 */
+	public static Graph jsonToGraph(String fileName) {
 		Graph graph = new Graph();
 		
-		try {
+		try {			
 			JSONObject doc = (JSONObject) JSONValue.parse(new FileReader(fileName));
     		JSONArray nodes = (JSONArray) doc.get("nodes");
     		@SuppressWarnings("unchecked")
 			Iterator<JSONObject> iter = nodes.iterator();
 			while(iter.hasNext()){
 				JSONObject node = iter.next();
+				
 				String nodeName = (String) node.get("name");
 				String application = (String) node.get("application");
 				int level = new BigDecimal((Long)node.get("level")).intValue();
@@ -65,25 +71,29 @@ public class Bridge {
 				double velocity = (Double) node.get("velocity");
 				
 				Movement movement = new Movement(velocity, direction, new Location(posX, posY));
-				
-				int distType = new BigDecimal((Long)node.get("distribution")).intValue();
 				Distribution distribution = null;
 				
-				if(distType == Distribution.DETERMINISTIC)
-					distribution = new DeterministicDistribution(new BigDecimal((Double)node.get("value")).doubleValue());
-				else if(distType == Distribution.NORMAL){
-					distribution = new NormalDistribution(new BigDecimal((Double)node.get("mean")).doubleValue(), 
-							new BigDecimal((Double)node.get("stdDev")).doubleValue());
-				} else if(distType == Distribution.UNIFORM){
-					distribution = new UniformDistribution(new BigDecimal((Double)node.get("min")).doubleValue(), 
-							new BigDecimal((Double)node.get("max")).doubleValue());
+				try {
+					int distType = new BigDecimal((Long)node.get("distribution")).intValue();
+					
+					if(distType == Distribution.DETERMINISTIC)
+						distribution = new DeterministicDistribution(new BigDecimal((Double)node.get("value")).doubleValue());
+					else if(distType == Distribution.NORMAL){
+						distribution = new NormalDistribution(new BigDecimal((Double)node.get("mean")).doubleValue(), 
+								new BigDecimal((Double)node.get("stdDev")).doubleValue());
+					} else if(distType == Distribution.UNIFORM){
+						distribution = new UniformDistribution(new BigDecimal((Double)node.get("min")).doubleValue(), 
+								new BigDecimal((Double)node.get("max")).doubleValue());
+					}
+				} catch (Exception e) {
+					// Do nothing
 				}
 
 				Node fogDevice = new FogDeviceGui(nodeName, level, mips, ram, strg, rateMips, rateRam, rateStorage, rateBw, rateEnergy, idlePower,
 						busyPower, movement, application, distribution);
 				graph.addNode(fogDevice);
-			}
-				
+			}		
+			
 			JSONArray links = (JSONArray) doc.get("links");
 			@SuppressWarnings("unchecked")
 			Iterator<JSONObject> linksIter =links.iterator(); 
@@ -107,8 +117,8 @@ public class Bridge {
 			@SuppressWarnings("unchecked")
 			Iterator<JSONObject> appsIter = applications.iterator(); 
 			while(appsIter.hasNext()){
-				JSONObject app = appsIter.next();				
-				graph.addApp(new ApplicationGui((String) app.get("name"), app.get("loops")));
+				JSONObject app = appsIter.next();
+				graph.addApp(new Application((String) app.get("name")));
 			}
 			
 			JSONArray modules = (JSONArray) doc.get("modules");
@@ -120,13 +130,12 @@ public class Bridge {
 				String appId = (String) model.get("appId");
 				String name = (String) model.get("name");
 				int ram = ((Long) model.get("ram")).intValue();
-				long strg = (Long) model.get("strg");
 				boolean clientModule = (Boolean) model.get("clientModule");
 				boolean globalModule = (Boolean) model.get("globalModule");
 				
-				for(ApplicationGui app : graph.getAppList()) {
+				for(Application app : graph.getAppList()) {
 					if(app.getAppId().equals(appId)) {
-						app.addAppModule(name, ram, strg, clientModule, globalModule);
+						app.addAppModule(name, ram, clientModule, globalModule);
 						break;
 					}
 				}
@@ -148,7 +157,7 @@ public class Bridge {
 				String tupleType = (String) edge.get("tupleType");
 				int edgeType = ((Long) edge.get("edgeType")).intValue();
 				
-				for(ApplicationGui app : graph.getAppList()) {
+				for(Application app : graph.getAppList()) {
 					if(app.getAppId().equals(appId)) {
 						if(!periodic)
 							app.addAppEdge(source, destination, tupleCpuLength, tupleNwLength, tupleType,
@@ -156,6 +165,28 @@ public class Bridge {
 						else
 							app.addAppEdge(source, destination, periodicity,tupleCpuLength, tupleNwLength,
 									tupleType, edgeType);
+						break;
+					}
+				}
+			}
+			
+			JSONArray loops = (JSONArray) doc.get("loops");
+			@SuppressWarnings("unchecked")
+			Iterator<JSONObject> loopsIter = loops.iterator(); 
+			while(loopsIter.hasNext()){
+				JSONObject loop = loopsIter.next();
+				
+				@SuppressWarnings("unchecked")
+				List<String> moduleList = (List<String>) loop.get("modules");
+				double deadline = (Double) loop.get("deadline");
+				String appId = (String) loop.get("appId");
+				
+				for(Application app : graph.getAppList()) {
+					if(app.getAppId().equals(appId)) {
+						final AppLoop l = new AppLoop(moduleList, deadline);
+						List<AppLoop> listLoops = app.getLoops();
+						listLoops.add(l);
+						app.setLoops(listLoops);
 						break;
 					}
 				}
@@ -173,7 +204,7 @@ public class Bridge {
 				String outputTuple = (String) tuple.get("outputTuple");
 				double selectivity = (Double) tuple.get("selectivity");
 				
-				for(ApplicationGui app : graph.getAppList()) {
+				for(Application app : graph.getAppList()) {
 					if(app.getAppId().equals(appId)) {
 						app.addTupleMapping(moduleName, inputTuple, outputTuple, new FractionalSelectivity(selectivity));
 						break;
@@ -188,9 +219,14 @@ public class Bridge {
 		return graph;
 	}
 	
-	// convert from Graph object to JSON object
+	/**
+	 * Converts from Graph object to JSON object.
+	 * 
+	 * @param graph the object which holds the physical topology
+	 * @return the JSON result
+	 */
 	@SuppressWarnings("unchecked")
-	public static String graphToJson(Graph graph){
+	public static String graphToJson(Graph graph) {
 		if(graph.getDevicesList().size() < 1 && graph.getAppList().size() < 1)
 			return "Graph is Empty";
 
@@ -224,20 +260,26 @@ public class Bridge {
 			jobj.put("direction", fogDevice.getMovement().getDirection());
 			jobj.put("velocity", fogDevice.getMovement().getVelocity());
 			jobj.put("application", fogDevice.getApplication());
-			jobj.put("distribution", fogDevice.getDistributionType());
 			
-			if(fogDevice.getDistributionType()==Distribution.DETERMINISTIC)
-				jobj.put("value", ((DeterministicDistribution)fogDevice.getDistribution()).getValue());
-			else if(fogDevice.getDistributionType()==Distribution.NORMAL){
-				jobj.put("mean", ((NormalDistribution)fogDevice.getDistribution()).getMean());
-				jobj.put("stdDev", ((NormalDistribution)fogDevice.getDistribution()).getStdDev());
-			} else if(fogDevice.getDistributionType()==Distribution.UNIFORM){
-				jobj.put("min", ((UniformDistribution)fogDevice.getDistribution()).getMin());
-				jobj.put("max", ((UniformDistribution)fogDevice.getDistribution()).getMax());
+			try {
+				jobj.put("distribution", fogDevice.getDistributionType());
+				
+				if(fogDevice.getDistributionType()==Distribution.DETERMINISTIC)
+					jobj.put("value", ((DeterministicDistribution)fogDevice.getDistribution()).getValue());
+				else if(fogDevice.getDistributionType()==Distribution.NORMAL){
+					jobj.put("mean", ((NormalDistribution)fogDevice.getDistribution()).getMean());
+					jobj.put("stdDev", ((NormalDistribution)fogDevice.getDistribution()).getStdDev());
+				} else if(fogDevice.getDistributionType()==Distribution.UNIFORM){
+					jobj.put("min", ((UniformDistribution)fogDevice.getDistribution()).getMin());
+					jobj.put("max", ((UniformDistribution)fogDevice.getDistribution()).getMax());
+				}
+			} catch (Exception e) {
+				// Do nothing
 			}
+			
 			nodes.add(jobj);
 			
-			// add edge
+			// Add edge
 			for (Link edge : entry.getValue()) {
 				Node destNode = edge.getNode();
 
@@ -251,7 +293,7 @@ public class Bridge {
 				jobj2.put("bw", edge.getBandwidth());
 				links.add(jobj2);
 				
-				// add exist edge to the edgeList
+				// Add exist edge to the edgeList
 				if (edgeList.containsKey(entry.getKey()))
 					edgeList.get(entry.getKey()).add(edge.getNode());
 				else {
@@ -264,23 +306,23 @@ public class Bridge {
 		topo.put("nodes", nodes);
 		topo.put("links", links);
 		
-		// add application
+		// Add application
 		JSONArray applications = new JSONArray();
 		JSONArray modules = new JSONArray();
 		JSONArray edges = new JSONArray();
 		JSONArray tuples = new JSONArray();
-		for (ApplicationGui applicationGui : graph.getAppList()) {
+		JSONArray loops = new JSONArray();
+		
+		for (Application application : graph.getAppList()) {
 			JSONObject jobj = new JSONObject();
-			jobj.put("name", applicationGui.getAppId());
-			jobj.put("loops", applicationGui.getLoops());
+			jobj.put("name", application.getAppId());
 			applications.add(jobj);
 			
-			for(AppModule appModule : applicationGui.getModules()) {
+			for(AppModule appModule : application.getModules()) {
 				jobj = new JSONObject();
 				jobj.put("appId", appModule.getAppId());
 				jobj.put("name", appModule.getName());
 				jobj.put("ram", appModule.getRam());
-				jobj.put("strg", appModule.getSize());
 				jobj.put("clientModule", appModule.isClientModule());
 				jobj.put("globalModule", appModule.isGlobalModule());
 				modules.add(jobj);
@@ -296,9 +338,9 @@ public class Bridge {
 				}
 			}
 			
-			for(AppEdge appEdge : applicationGui.getEdges()) {
+			for(AppEdge appEdge : application.getEdges()) {
 				jobj = new JSONObject();
-				jobj.put("appId", applicationGui.getAppId());
+				jobj.put("appId", application.getAppId());
 				jobj.put("source", appEdge.getSource());
 				jobj.put("destination", appEdge.getDestination());
 				jobj.put("periodic", appEdge.isPeriodic());
@@ -309,12 +351,21 @@ public class Bridge {
 				jobj.put("edgeType", appEdge.getEdgeType());
 				edges.add(jobj);
 			}
+			
+			for(AppLoop appLoop : application.getLoops()) {
+				jobj = new JSONObject();
+				jobj.put("appId", application.getAppId());
+				jobj.put("modules", appLoop.getModules());
+				jobj.put("deadline", appLoop.getDeadline());
+				loops.add(jobj);
+			}
 		}
 		
 		topo.put("applications", applications);
 		topo.put("modules", modules);
 		topo.put("edges", edges);
 		topo.put("tuples", tuples);
+		topo.put("loops", loops);
 		
 		StringWriter out = new StringWriter();
 		String jsonText = "";
@@ -328,5 +379,19 @@ public class Bridge {
 		
 		//System.out.println(jsonText);
 		return jsonText;
+	}
+	
+	/**
+	 * Gets a node from the graph by its name.
+	 * 
+	 * @param graph the object which holds the physical topology
+	 * @param name the name of the node
+	 * @return the node
+	 */
+	private static Node getNode(Graph graph, String name){
+		for(Node node : graph.getDevicesList().keySet())
+			if(node != null && node.getName().equals(name))
+				return node;
+		return null;
 	}
 }
