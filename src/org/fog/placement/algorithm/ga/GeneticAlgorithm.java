6924084cd4a67a4c71519de1abd959b3097f0254
JoseCVieira <josecarlosvieira@tecnico.ulisboa.pt>
2019-08-16 18:06:03 +0100

    Organizing and commenting code

--- src/org/fog/placement/algorithm/ga/GeneticAlgorithm.java
+++ src/org/fog/placement/algorithm/ga/GeneticAlgorithm.java
@@ -1,4 +1,4 @@
-package org.fog.placement.algorithm.overall.ga;
+package org.fog.placement.algorithm.ga;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,14 +12,31 @@ import org.fog.entities.FogDevice;
 import org.fog.entities.Sensor;
 import org.fog.placement.algorithm.Algorithm;
 import org.fog.placement.algorithm.Job;
-import org.fog.placement.algorithm.overall.util.AlgorithmUtils;
 
+/**
+ * Class in which defines and executes the genetic algorithm.
+ * 
+ * @author José Carlos Ribeiro Vieira @ Instituto Superior Técnico (IST)
+ * @since  July, 2019
+ */
 public class GeneticAlgorithm extends Algorithm {
-	private static final int FITTEST = (int)((10*Config.POPULATION_SIZE_GA)/100);  // 10% of fittest population
+	/** 10% of the population */
+	private static final int FITTEST = (int)(Config.POPULATION_SIZE_GA*0.1);
 	
+	/** Best solution found by the algorithm */
 	private Job bestSolution;
+	
+	/** Best cost found by the algorithm */
 	private double bestCost;
+	
+	/** Current iteration of the algorithm */
 	private int iteration;
+	
+	/** Time at the beginning of the execution of the algorithm */
+	private long start;
+	
+	/** Time at the end of the execution of the algorithm */
+	private long finish;
 	
 	public GeneticAlgorithm(final List<FogDevice> fogDevices, final List<Application> applications,
 			final List<Sensor> sensors, final List<Actuator> actuators) {
@@ -28,137 +45,184 @@ public class GeneticAlgorithm extends Algorithm {
 	
 	@Override
 	public Job execute() {
-		bestSolution = null;
-		bestCost = Constants.REFERENCE_COST;
 		iteration = 0;
+		bestCost = Constants.REFERENCE_COST;
+		bestSolution = null;
 		
+		// Time at the beginning of the execution of the algorithm
+		start = System.currentTimeMillis();
+		
+		// Generate the Dijkstra graph
+		generateDijkstraGraph();
+		
+		// Solve the problem
+		solveModulePlacement();
+	    
+	    // Time at the end of the execution of the algorithm
+ 		finish = System.currentTimeMillis();
+ 		
+ 		setElapsedTime(finish - start);
+ 		
+ 		return bestSolution;
+	}
+			
+	/**
+	 * Solves the module placement map.
+	 */
+	private void solveModulePlacement() {
+		int convergenceIter = 0;
+		int generation = 0;
 		Individual[] population = new Individual[Config.POPULATION_SIZE_GA];
 		
-		int convergenceIter = 0;
-		int generation = 1;
-		
-		long start = System.currentTimeMillis();
-		
+		// Generate an initial population with random module placements
 	    for (int i = 0; i < Config.POPULATION_SIZE_GA; i++)
-	    	population[i] = new Individual(this, new Job(Job.generateRandomPlacement(this, NR_NODES, NR_MODULES)));
+	    	population[i] = new Individual(this, new Job(Job.generateRandomPlacement(this, getNumberOfNodes(), getNumberOfModules())));
 	    
 	    while (generation <= Config.MAX_ITER_PLACEMENT_GA) {
+	    	// Solve both tuple and virtual machine migration routing tables
 	    	population = GARouting(population);
 	    	
+	    	// Sort the array based on its value (ascending order)
 	    	Arrays.sort(population);
 	    	
-	    	double iterBest = population[0].getFitness();
+	    	// Get the best value found in the current generation
+	    	double iterGen = population[0].getFitness();
 			
-    		if(Math.abs(bestCost - iterBest) <= Config.CONVERGENCE_ERROR) {
+	    	// Check the convergence error
+    		if(Math.abs(bestCost - iterGen) <= Config.CONVERGENCE_ERROR) {
+    			// If it found the same (or similar) solution a given number of times in a row break the loop
 				if(++convergenceIter == Config.MAX_ITER_PLACEMENT_CONVERGENCE_GA)
 					generation = Config.MAX_ITER_PLACEMENT_GA + 1;
 			}else
     			convergenceIter = 0;
     		
-    		if(bestCost > iterBest) {
+    		// If it's a better solution, save and add it to the value/iteration map
+    		if(bestCost > iterGen) {
     			bestSolution = new Job(population[0].getChromosome());
-    			bestCost = iterBest;
+    			bestCost = iterGen;
 				
-				valueIterMap.put(iteration, bestCost);
+				getValueIterMap().put(iteration, bestCost);
     			
-				if(Config.PRINT_BEST_ITER)
+				if(Config.PRINT_ALGORITHM_ITER)
     				System.out.println("iteration: " + iteration + " value: " + bestCost);
     		}
     		
+    		// If the generation counter is above the defined maximum break the loop
     		if(generation > Config.MAX_ITER_PLACEMENT_GA) break;
 	  
-	        // otherwise generate new offsprings for new generation
+	        // Otherwise generate new offsprings for new generation
 	        Individual[] newGeneration = new Individual[Config.POPULATION_SIZE_GA];
 	        
+	        // Copy 10% of the fittest individuals to the next generation
 	        for(int i = 0; i < FITTEST; i++) {
 	        	newGeneration[i] = new Individual(this, new Job(population[i].getChromosome().getModulePlacementMap()));
 	        }
 	        
-	        // from 50% of fittest population, Individuals will mate to produce offspring
+	        // From 50% of fittest population, individuals will mate to produce offspring
 	        for(int i = FITTEST; i < Config.POPULATION_SIZE_GA; i++) {
 	        	int r1 = new Random().nextInt((int) (Config.POPULATION_SIZE_GA*0.5));
 	        	int r2 = new Random().nextInt((int) (Config.POPULATION_SIZE_GA*0.5));
+	        	
+	        	// Create the new individual
 	        	newGeneration[i] = new Individual(this, new Job(population[r1].matePlacement(population[r2])));
 	        }
 	        
-	        for(int i = 0; i < Config.POPULATION_SIZE_GA; i++) 
+	        // Set the current generation's population
+	        for(int i = 0; i < Config.POPULATION_SIZE_GA; i++) {
 	        	population[i] = newGeneration[i];
+	        }
 	        
+	     // Increments the generation for the module placement map
 	        generation++;
 	    }
-	    
-	    long finish = System.currentTimeMillis();
-	    elapsedTime = finish - start;
-	    
-	    if(Config.PRINT_DETAILS) {
-	    	AlgorithmUtils.printResults(this, bestSolution);
-	    }
-		
-		return bestSolution;
 	}
 	
+	/**
+	 * Solves both the tuple and virtual machine migration routing maps.
+	 * 
+	 * @param population the population containing only the module placement map
+	 * @return the population containing the whole solution (module placement map, and tuple and virtual machine migration routing maps)
+	 */
 	public Individual[] GARouting(Individual[] population) {
-		
+		// For each individual with a given module placement map
 		for (int i = 0; i < Config.POPULATION_SIZE_GA; i++) {
+			
+			// Get it's module placement map
 			int[][] modulePlacementMap = population[i].getChromosome().getModulePlacementMap();
+			
+			// Create a new population
 			Individual[] populationR = new Individual[Config.POPULATION_SIZE_GA];
 			
+			// Generate the new population with that module placement map and random tuple and virtual machine routing maps
 			for (int j = 0; j < Config.POPULATION_SIZE_GA; j++) {
-				int[][] tupleRoutingMap = Job.generateRandomTupleRouting(this, modulePlacementMap, NR_NODES);
-				int[][] migrationRoutingMap = Job.generateRandomMigrationRouting(this, modulePlacementMap, currentPlacement, NR_NODES, NR_MODULES);
+				int[][] tupleRoutingMap = Job.generateRandomTupleRouting(this, modulePlacementMap, getNumberOfNodes(), getNumberOfDependencies());
+				int[][] migrationRoutingMap = Job.generateRandomMigrationRouting(this, modulePlacementMap, getNumberOfNodes(), getNumberOfModules());
 				populationR[j] = new Individual(this, new Job(this, modulePlacementMap, tupleRoutingMap, migrationRoutingMap));
 			}
 			
-			int generation = 1;
 			double bestValue = Constants.REFERENCE_COST;
 			int convergenceIter = 0;
+			int generation = 0;
 			
 			while (generation <= Config.MAX_ITER_ROUTING_GA) {
+				// Sort the array based on its value (ascending order)
 	    		Arrays.sort(populationR);
 	    		
-	    		double iterBest = populationR[0].getFitness();
+	    		// Get the best value found in the current generation
+		    	double iterGen = populationR[0].getFitness();
     			
-	    		if(Math.abs(bestValue - iterBest) <= Constants.EPSILON) {
-    				if(++convergenceIter == Config.MAX_ITER_ROUTING_CONVERGENCE_GA) {
+		    	// Check the convergence error
+	    		if(Math.abs(bestValue - iterGen) <= Constants.EPSILON) {
+	    			// If it found the same (or similar) solution a given number of times in a row break the loop
+    				if(++convergenceIter == Config.MAX_ITER_ROUTING_CONVERGENCE_GA)
     					generation = Config.MAX_ITER_PLACEMENT_GA + 1;
-    				}
     			}else
 	    			convergenceIter = 0;
 	    		
-	    		if(bestValue > iterBest) {
-    				bestValue = iterBest;
+	    		// Save the best value for that module placement
+	    		if(bestValue > iterGen) {
+    				bestValue = iterGen;
 	    		}
 	    		
+	    		// If the generation counter is above the defined maximum break the loop
 	    		if(generation > Config.MAX_ITER_PLACEMENT_GA) break;
 	    		
+	    		// Otherwise generate new offsprings for new generation
 		        Individual[] newGeneration = new Individual[Config.POPULATION_SIZE_GA];
 		        
+		        // Copy 10% of the fittest individuals to the next generation
 		        for(int z = 0; z < FITTEST; z++)
 		        	newGeneration[z] = populationR[z];
 		        
+		        // From 50% of fittest population, individuals will mate to produce offspring
 		        for(int z = FITTEST; z < Config.POPULATION_SIZE_GA; z++) {
 		        	int r1 = new Random().nextInt((int) (Config.POPULATION_SIZE_GA*0.5));
 		        	int r2 = new Random().nextInt((int) (Config.POPULATION_SIZE_GA*0.5));
-		        	newGeneration[z] = populationR[r1].mateRouting(populationR[r2]);
+		        	
+		        	int[][] childTupleRoutingMap = populationR[r1].mateTupleRouting(populationR[r2]);
+		        	int[][] childMigrationRoutingMap = populationR[r1].mateMigrationRouting(populationR[r2]);
+		        	
+		        	// Create the new individual
+		        	newGeneration[z] = new Individual(this, new Job(this, modulePlacementMap, childTupleRoutingMap, childMigrationRoutingMap));
 		        }
 		        
-		        for(int z = 0; z < Config.POPULATION_SIZE_GA; z++) 
+		        // Set the current generation's population
+		        for(int z = 0; z < Config.POPULATION_SIZE_GA; z++)
 		        	populationR[z] = newGeneration[z];
 		        
+		        // Increments the number of iterations for the whole genetic algorithm
 		        iteration++;
+		        
+		        // Increments the generation for the tuple and virtual machine routing maps
 		        generation++;
 			}
 			
+			// After running the genetic algorithm for the tuple and virtual machine routing maps (that module placement),
+			// copy the best one to the final population
 			population[i] = populationR[0];
 		}
 		
 		return population;
-	}
-	
-	public Individual[] GAMigrationRouting(Individual[] population) {
-		
-		
-		return null;
 	}
 	
 }
--- src/org/fog/placement/algorithm/ga/Individual.java
+++ src/org/fog/placement/algorithm/ga/Individual.java
@@ -1,30 +1,47 @@
-package org.fog.placement.algorithm.overall.ga;
+package org.fog.placement.algorithm.ga;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 import org.fog.core.Constants;
 import org.fog.placement.algorithm.Job;
-import org.fog.placement.algorithm.overall.util.AlgorithmUtils;
-import org.fog.placement.algorithm.routing.DijkstraAlgorithm;
-import org.fog.placement.algorithm.routing.Edge;
-import org.fog.placement.algorithm.routing.Graph;
-import org.fog.placement.algorithm.routing.Vertex;
 
-public class Individual implements Comparable<Individual> {	
+/**
+ * Class representing individuals used within the genetic algorithm.
+ * 
+ * @author José Carlos Ribeiro Vieira @ Instituto Superior Técnico (IST), Lisbon-Portugal
+ * @since  July, 2019
+ */
+public class Individual implements Comparable<Individual> {
+	/** Object which holds all the information needed to run the optimization algorithm */
 	private GeneticAlgorithm ga;
+	
+	/** Object which contains the problem solution for the individual */
 	private Job chromosome;
+	
+	/** Result of the cost function */
 	private double fitness;
 	
+	/**
+	 * Creates a new individual with a given chromosome.
+	 * 
+	 * @param ga the object which holds all the information needed to run the optimization algorithm
+	 * @param chromosome the problem solution
+	 */
 	Individual(GeneticAlgorithm ga, Job chromosome) {
 		this.ga = ga;
 		this.chromosome = chromosome;
 		this.fitness = chromosome.getCost();
 	}
 	
-	// perform mating and produce new offspring
+	/**
+	 * Performs mating and produce new offspring.
+	 * 
+	 * @param par the individual which the mate will occur
+	 * @return the module placement offspring
+	 */
 	public int[][] matePlacement(Individual par) {
 		int[][] modulePlacementMap = chromosome.getModulePlacementMap();
 		int[][] parModulePlacementMap = par.getChromosome().getModulePlacementMap();
@@ -33,13 +50,13 @@ public class Individual implements Comparable<Individual> {
 		for(int i = 0; i < modulePlacementMap[0].length; i++) {
         	float prob = new Random().nextFloat();
         	
-        	// if prob is less than 0.45, insert gene from parent 1
+        	// If probability is less than 0.45, insert gene from one of the parents
             if (prob < 0.45)
             	childModulePlacementMap[Job.findModulePlacement(modulePlacementMap, i)][i] = 1;
-            // if prob is between 0.45 and 0.90, insert gene from parent 2
+            // If probability is between 0.45 and 0.90, insert gene from the other parent
             else if (prob < 0.90)
             	childModulePlacementMap[Job.findModulePlacement(parModulePlacementMap, i)][i] = 1;
-            // otherwise insert random gene(mutate), for maintaining diversity
+            // Otherwise insert random gene(mutate), for maintaining diversity
             else {
             	double[][] possibleDeployment = ga.getPossibleDeployment();
         		
@@ -58,197 +75,137 @@ public class Individual implements Comparable<Individual> {
 		return childModulePlacementMap;
 	}
 	
-	public Individual mateRouting(Individual par) {
-		int[][] modulePlacementMap = getChromosome().getModulePlacementMap();
-		int[][] currentPositionInt = ga.getCurrentPositionInt();
-		
+	/**
+	 * Performs mating of tuple routing map and produce new offspring.
+	 * 
+	 * @param par the individual which the mate will occur
+	 * @return the tuple routing map offspring
+	 */
+	public int[][] mateTupleRouting(Individual par) {
+		int nrFogNodes = ga.getNumberOfNodes();
+		int nrDependencies = ga.getNumberOfDependencies();
+		int[][] modulePlacementMap = chromosome.getModulePlacementMap();
 		int[][] tupleRoutingMap = chromosome.getTupleRoutingMap();
 		int[][] parTupleRoutingMap = par.getChromosome().getTupleRoutingMap();
-		int[][] migrationRoutingMap = chromosome.getMigrationRoutingMap();
-		int[][] parMigrationRoutingMap = par.getChromosome().getMigrationRoutingMap();
+		int[][] childTupleRoutingMap = new int[nrDependencies][nrFogNodes];
 		
-		int[][] childTupleRoutingMap = new int[tupleRoutingMap.length][tupleRoutingMap[0].length];
-		int[][] childMigrationRoutingMap = new int[migrationRoutingMap.length][migrationRoutingMap[0].length];
-		
-		List<Integer> tupleInitialNodes = new ArrayList<Integer>();
-		List<Integer> tupleFinalNodes = new ArrayList<Integer>();
-		List<Integer> migrationInitialNodes = new ArrayList<Integer>();
-		List<Integer> migrationFinalNodes = new ArrayList<Integer>();
-		List<Vertex> nodes = new ArrayList<Vertex>();
-		List<Edge> edges = new ArrayList<Edge>();
-		
-		int nrFogNodes = ga.getNumberOfNodes();
-		int nrModule = ga.getNumberOfModules();
-		
-		for(int i = 0; i < ga.getmDependencyMap().length; i++) {
-			for(int j = 0; j < ga.getmDependencyMap()[0].length; j++) {
-				if(ga.getmDependencyMap()[i][j] != 0) {
-					tupleInitialNodes.add(Job.findModulePlacement(modulePlacementMap, i));
-					tupleFinalNodes.add(Job.findModulePlacement(modulePlacementMap, j));
+		for(int i = 0; i < nrDependencies; i++) {
+			float prob = new Random().nextFloat();
+			
+			// If probability is less than 0.45, insert gene from one of the parents
+			if (prob < 0.45)
+				childTupleRoutingMap[i] = Arrays.copyOf(tupleRoutingMap[i], nrFogNodes);
+			// If probability is between 0.45 and 0.90, insert gene from the other parent
+			else if (prob < 0.9)
+				childTupleRoutingMap[i] = Arrays.copyOf(parTupleRoutingMap[i], nrFogNodes);
+			// Otherwise insert random gene(mutate), for maintaining diversity
+			else {
+				childTupleRoutingMap[i][0] = Job.findModulePlacement(modulePlacementMap, ga.getStartModDependency(i));
+				childTupleRoutingMap[i][nrFogNodes-1] = Job.findModulePlacement(modulePlacementMap, ga.getFinalModDependency(i));
+		        
+				for(int j = 1; j < nrFogNodes - 1; j++) {
+					// If its already the final node, then just fill the remain ones
+					if(childTupleRoutingMap[i][j-1] == childTupleRoutingMap[i][nrFogNodes-1]) {
+						for(; j < nrFogNodes - 1; j++) {
+							childTupleRoutingMap[i][j] = childTupleRoutingMap[i][nrFogNodes-1];
+						}
+						break;
+					}
+					
+					List<Integer> validValues = new ArrayList<Integer>();
+					
+					for(int z = 0; z < nrFogNodes; z++) {					
+						if(ga.getfLatencyMap()[childTupleRoutingMap[i][j-1]][z] == Constants.INF) continue;
+						if(!ga.isValidHop(z, childTupleRoutingMap[i][nrFogNodes-1], nrFogNodes - j)) continue;
+						validValues.add(z);
+					}
+							
+					childTupleRoutingMap[i][j] = validValues.get(new Random().nextInt(validValues.size()));
 				}
 			}
 		}
 		
-		for(int i = 0; i < nrModule; i++) {
-			if(ga.isFirstOptimization())
-				migrationInitialNodes.add(Job.findModulePlacement(modulePlacementMap, i));
-			else
-				migrationInitialNodes.add(Job.findModulePlacement(currentPositionInt, i));
-			migrationFinalNodes.add(Job.findModulePlacement(modulePlacementMap, i));
-		}
-		
-		for(int i  = 0; i < nrFogNodes; i++) {
-			nodes.add(new Vertex("Node=" + i));
-		}
-		
-		for(int i  = 0; i < nrFogNodes; i++) {
-			for(int j  = 0; j < nrFogNodes; j++) {
-				if(ga.getfLatencyMap()[i][j] < Constants.INF) {
-					 edges.add(new Edge(nodes.get(i), nodes.get(j), 1.0));
-				}
-			}
-        }
-		
-		for(int i = 0; i < tupleRoutingMap.length; i++) {
-			float prob = new Random().nextFloat();
-			
-			 if (prob < 0.45) {
-				 for (int j = 0; j < ga.getNumberOfNodes(); j++) {
-					 childTupleRoutingMap[i][j] = tupleRoutingMap[i][j];
-				 }
-			 }else if (prob < 0.9) {
-				 for (int j = 0; j < ga.getNumberOfNodes(); j++) {
-					 childTupleRoutingMap[i][j] = parTupleRoutingMap[i][j];
-				 }
-			 }else {
-				 childTupleRoutingMap[i][0] = tupleInitialNodes.get(i);
-				 childTupleRoutingMap[i][ga.getNumberOfNodes()-1] = tupleFinalNodes.get(i);
-				 
-				 Graph graph = new Graph(nodes, edges);
-				 DijkstraAlgorithm dijkstra = new DijkstraAlgorithm(graph);
-				 dijkstra.execute(nodes.get(tupleFinalNodes.get(i)));
-				
-				 for(int j = 1; j < nrFogNodes - 1; j++) {
-					 List<Integer> validValues = new ArrayList<Integer>();
-					
-					 for(int z = 0; z < nrFogNodes; z++) {
-						 
-						 // If there is a connection with the previous vertex
-						 if(ga.getfLatencyMap()[childTupleRoutingMap[i][j-1]][z] < Constants.INF) {
-							 
-							 LinkedList<Vertex> path = dijkstra.getPath(nodes.get(z));
-							 
-							 // If is possible to connect with the final node
-							 if(path != null && path.size() <= nrFogNodes - j) {
-								 validValues.add(z);
-							 }
-							 
-							 validValues.add(childTupleRoutingMap[i][j-1]);
-						 }
-					 }
-					 
-					 childTupleRoutingMap[i][j] = validValues.get(new Random().nextInt(validValues.size()));
-				 }
-			 }
-		}
-		
-		
-		
-		for (int i = 0; i < migrationRoutingMap.length; i++) {
-			float prob = new Random().nextFloat();
-			
-			 if (prob < 0.45) {
-				 for (int j = 0; j < ga.getNumberOfNodes(); j++) {
-					 childMigrationRoutingMap[i][j] = migrationRoutingMap[i][j];
-				 }
-			 }else if (prob < 0.9) {
-				 for (int j = 0; j < ga.getNumberOfNodes(); j++) {
-					 childMigrationRoutingMap[i][j] = parMigrationRoutingMap[i][j];
-				 }
-			 }else {
-				 childMigrationRoutingMap[i][0] = migrationInitialNodes.get(i);
-				 childMigrationRoutingMap[i][ga.getNumberOfNodes()-1] = migrationFinalNodes.get(i);
-				 
-				 Graph graph = new Graph(nodes, edges);
-				 DijkstraAlgorithm dijkstra = new DijkstraAlgorithm(graph);
-				 dijkstra.execute(nodes.get(migrationFinalNodes.get(i)));
-				
-				 for(int j = 1; j < nrFogNodes - 1; j++) {
-					 List<Integer> validValues = new ArrayList<Integer>();
-					
-					 for(int z = 0; z < nrFogNodes; z++) {
-						 
-						 // If there is a connection with the previous vertex
-						 if(ga.getfLatencyMap()[childMigrationRoutingMap[i][j-1]][z] < Constants.INF) {
-							 
-							 LinkedList<Vertex> path = dijkstra.getPath(nodes.get(z));
-							 
-							 // If is possible to connect with the final node
-							 if(path != null && path.size() <= nrFogNodes - j) {
-								 validValues.add(z);
-							 }
-							 
-							 validValues.add(childMigrationRoutingMap[i][j-1]);
-						 }
-					 }
-					 
-					 childMigrationRoutingMap[i][j] = validValues.get(new Random().nextInt(validValues.size()));
-				 }
-			 }
-		}
-		
-		Job childChromosome = new Job(ga, modulePlacementMap, childTupleRoutingMap, childMigrationRoutingMap);
-		
-        return new Individual(ga, childChromosome);
+		return childTupleRoutingMap;
 	}
 	
+	/**
+	 * Performs mating of virtual machine migration routing map and produce new offspring.
+	 * 
+	 * @param par the individual which the mate will occur
+	 * @return the virtual machine migration routing map offspring
+	 */
+	public int[][] mateMigrationRouting(Individual par) {
+		int nrFogNodes = ga.getNumberOfNodes();
+		int nrModules = ga.getNumberOfModules();
+		int[][] modulePlacementMap = chromosome.getModulePlacementMap();
+		int[][] currentPositionInt = ga.getCurrentPositionInt();
+		int[][] migrationRoutingMap = chromosome.getMigrationRoutingMap();
+		int[][] parMigrationRoutingMap = par.getChromosome().getMigrationRoutingMap();
+		int[][] childMigrationRoutingMap = new int[nrModules][nrFogNodes];
+		
+		for (int i = 0; i < nrModules; i++) {
+			float prob = new Random().nextFloat();
+			
+			// If probability is less than 0.45, insert gene from one of the parents
+			if (prob < 0.45)
+				childMigrationRoutingMap[i] = Arrays.copyOf(migrationRoutingMap[i], nrFogNodes);
+			// If probability is between 0.45 and 0.90, insert gene from the other parent
+			else if (prob < 0.9)
+				childMigrationRoutingMap[i] = Arrays.copyOf(parMigrationRoutingMap[i], nrFogNodes);
+			// Otherwise insert random gene(mutate), for maintaining diversity
+			else {
+				childMigrationRoutingMap[i][0] = Job.findModulePlacement(ga.isFirstOptimization() ? modulePlacementMap : currentPositionInt, i);
+				childMigrationRoutingMap[i][nrFogNodes-1] = Job.findModulePlacement(modulePlacementMap, i);
+					
+				for(int j = 1; j < nrFogNodes - 1; j++) { // Routing hop index
+					// If its already the final node, then just fill the remain ones
+					if(childMigrationRoutingMap[i][j-1] == childMigrationRoutingMap[i][nrFogNodes-1]) {
+						for(; j < nrFogNodes - 1; j++) {
+							childMigrationRoutingMap[i][j] = childMigrationRoutingMap[i][nrFogNodes-1];
+						}
+						break;
+					}
+						
+					List<Integer> validValues = new ArrayList<Integer>();
+						
+					for(int z = 0; z < nrFogNodes; z++) { // Node index
+						if(ga.getfLatencyMap()[childMigrationRoutingMap[i][j-1]][z] == Constants.INF) continue;
+						if(!ga.isValidHop(z, childMigrationRoutingMap[i][nrFogNodes-1], nrFogNodes - j)) continue;
+						validValues.add(z);
+					}
+						
+					childMigrationRoutingMap[i][j] = validValues.get(new Random().nextInt(validValues.size()));
+				}
+			}
+		}
+		
+		return childMigrationRoutingMap;
+	}
+	
+	/**
+	 * Compares two individuals based on it's fitness value (used to sort arrays of individuals).
+	 */
 	@Override
 	public int compareTo(Individual individual) {
 		return Double.compare(this.getFitness(), individual.getFitness());
 	}
-
-	double getFitness() {
-		return fitness;
-	}
 	
+	/**
+	 * Gets the object which contains the problem solution for the individual.
+	 * 
+	 * @return the object which contains the problem solution for the individual
+	 */
 	Job getChromosome() {
 		return chromosome;
 	}
 	
-	@Override
-	public String toString() {
-		int[][] modulePlacementMap = chromosome.getModulePlacementMap();
-		int[][] routingMap = chromosome.getTupleRoutingMap();
-		
-		String toReturn = "\nChromosome: \n";
-		
-		for(int i = 0; i < modulePlacementMap.length; i++) {
-			for(int j = 0; j < modulePlacementMap[i].length; j++)
-				toReturn += modulePlacementMap[i][j] + " ";
-			toReturn += "\n";
-		}
-		toReturn += "\n";
-		
-		int iter = 0;
-		for(int i = 0; i < ga.getmDependencyMap().length; i++) {
-			for(int j = 0; j < ga.getmDependencyMap()[i].length; j++) {
-				if(ga.getmDependencyMap()[i][j] != 0) {
-					toReturn += AlgorithmUtils.centerString(40, "[" + ga.getmName()[i] + " -> " + ga.getmName()[j] + "]:");
-					
-					for(int z = 0; z < routingMap[iter].length; z++) {
-						if(z < routingMap[iter].length - 1)
-							toReturn += AlgorithmUtils.centerString(10, ga.getfName()[(int) routingMap[iter][z]]) + " -> ";
-						else
-							toReturn += AlgorithmUtils.centerString(10, ga.getfName()[(int) routingMap[iter][z]]) + "\n";
-					}
-					iter++;
-				}
-			}
-		}
-		
-		toReturn += "\nAnd it has fitness= " + getFitness() + "";
-		
-		return toReturn;
+	/**
+	 * Gets the result of the cost function.
+	 * 
+	 * @return the result of the cost function
+	 */
+	double getFitness() {
+		return fitness;
 	}
 	
 }
